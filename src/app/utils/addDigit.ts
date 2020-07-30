e38fbde3ebb747e5f2860c9cf302ec5d48f85270
Maciej Szczepanski <maciek2712@interia.pl>
2020-07-30 18:02:14 +0200

    showResult update

--- src/app/utils/addDigit.ts
+++ src/app/utils/addDigit.ts
@@ -3,5 +3,6 @@ import { GlobalObj } from "../global/types";
 const addDigit = (e: Event, obj: GlobalObj) => {
   const digit = (<HTMLButtonElement>e.target).value;
   obj.action === "" ? (obj.num1 += digit) : (obj.num2 += digit);
+  console.log(obj);
 };
 export default addDigit;
--- src/app/utils/showResult.ts
+++ src/app/utils/showResult.ts
@@ -1,7 +1,9 @@
-import { GlobalObj, DisplayObj } from "../global/types";
-
-const showResult = (obj: GlobalObj, obj2: DisplayObj) => {
+const showResult = (
+  obj: { num1: string; num2: string; action: string; result: string },
+  obj2: { numberDisplay: Element | null; actionDisplay: Element | null }
+) => {
   const { result, action, num1, num2 } = obj;
+  console.log(obj);
 
   if (obj2.numberDisplay) {
     if (num2 === "" && result == "") obj2.numberDisplay.innerHTML = num1;
@@ -13,13 +15,10 @@ const showResult = (obj: GlobalObj, obj2: DisplayObj) => {
     if (result === "") obj2.actionDisplay.innerHTML = action;
     else obj2.actionDisplay.innerHTML = "";
   }
+
+  if (obj2.numberDisplay && obj2.numberDisplay.innerHTML === "") {
+    obj2.numberDisplay.innerHTML = "0";
+  }
 };
 
 export default showResult;
-
-/*
- const resultLength = obj.result.toString().length;
-  if (resultLength > 12) container.innerHTML = result.toPrecision(8);
-  else container.innerHTML = result;
-
-  */
